d137e387fd4853cd925dcc1b196049551c4f1c8d
Neha Kothari <sneha.kothari@gmail.com>
2014-10-01 16:16:48 -0700

    First revision

--- AgeofLaika/ViewController.swift
+++ AgeofLaika/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var enterHumanYearsTextField: UITextField!
+    @IBOutlet weak var convertedYearsLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func converttoDogYearsButtonPressed(sender: UIButton) {
+        convertedYearsLabel.hidden = false
+        enterHumanYearsTextField.resignFirstResponder()
+        let convertedYearsDouble = Double((enterHumanYearsTextField.text as NSString).doubleValue) * 7
+        convertedYearsLabel.text = "\(enterHumanYearsTextField.text) human years equals \(convertedYearsDouble) dog years"
+        convertedYearsLabel.textColor = UIColor.blueColor()
+        
+    }
 
 }
 
